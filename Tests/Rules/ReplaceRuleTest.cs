27685bcb57a8e6abaa5ee0b02cf1e342ea468ba8
AllenJB <allenjb@users.noreply.github.com>
2026-07-29 09:53:19 +0000

    Initial tests for ReplaceRule

---
+++ Tests/Rules/ReplaceRuleTest.cs
@@ -0,0 +1,30 @@
+﻿using JBRenamer.Rules;
+
+namespace Tests.Rules;
+
+public class ReplaceRuleTest
+{
+    [Fact]
+    public void SimpleReplace()
+    {
+        var underTest = new ReplaceRule("a", "b");
+        var testPath = (OperatingSystem.IsWindows() ? "C:/tmp/" : "/tmp/");
+
+        var ios = new List<(string, string)>
+        {
+            ("Test.txt", "Test.txt"),
+            ("Tast.txt", "Tbst.txt"),
+            ("A.txt", "A.txt"),
+            ("a.txt", "b.txt"),
+            ("AA.txt", "AA.txt"),
+            ("aa.txt", "bb.txt"),
+            ("test.a", "test.b"),
+        };
+
+        foreach ((string input, string expectedOutput) in ios)
+        {
+            var actualOutput = underTest.Run(testPath + input);
+            Assert.Equal(testPath + expectedOutput, actualOutput);
+        }
+    }
+}
